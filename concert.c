f386e72b8a71c5914702a6b7d14016f970f20242
Carlos <carlos.renteria@sjsu.com>
2014-02-25 08:56:41 -0800

    Initial Source
    #includes, some constants, mutex, semaphores

---
+++ concert.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#define SEAT_COUNT 100
+#define SEAT_ROWS 10
+#define SEAT_COLUMNS 10
+
+#define H_SELLERS 1
+#define M_SELLERS 3
+#define L_SELLERS 6
+
+#define MAX_WAITING_DURATION 10     //If person waits for 10 mins leaves.
+#define MAX_SELLING_DURATION 60     //Sell tickets for 60 mins
+
+char seats[SEAT_ROWS][SEAT_COLUMNS]; //Matrix of the auditorium
+
+pthread_mutex_t seatMutex;  //mutex protects the seats a (shared resource)
+sem_t filledH;              //seller H waits on this
+sem_t filledM;              //sellers M wait on this
+sem_t filledL;              //sellers L wait on this
+ 
+time_t startTime;
